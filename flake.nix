cb659dc675bc59dbc97fc0ad059782040c4f5ff3
John Bonini <bonini.physics@gmail.com>
2024-11-21 13:45:36 -0500

    nix + pdm packaging
    optional deps not tested

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Chips FF";
+
+  inputs = {
+    dream2nix.url = "github:nix-community/dream2nix";
+    nixpkgs.follows = "dream2nix/nixpkgs";
+  };
+
+  outputs = {
+    self,
+    dream2nix,
+    nixpkgs,
+  }: let
+    eachSystem = nixpkgs.lib.genAttrs [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+  in {
+    packages = eachSystem (system: {
+      default = self.packages.${system}.chipsff;
+      chipsff =
+        dream2nix.lib.evalModules {
+          packageSets.nixpkgs = nixpkgs.legacyPackages.${system};
+          modules = [
+            ./default.nix
+            {
+              paths.projectRoot = ./.;
+              paths.projectRootFile = "flake.nix";
+              paths.package = ./.;
+            }
+          ];
+        };
+    });
+    devShells = eachSystem (system: {
+      default = nixpkgs.legacyPackages.${system}.mkShell {
+        inputsFrom = [self.packages.${system}.chipsff.devShell];
+        packages = with nixpkgs.legacyPackages.${system}; [
+          ruff
+          pyright
+          self.packages.${system}.chipsff.config.deps.python.pkgs.ipython
+        ];
+      };
+    });
+  };
+}
