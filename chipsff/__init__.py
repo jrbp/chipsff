255d978310f485f84a33a05f9f712a9188f62463
John Bonini <bonini.physics@gmail.com>
2024-11-21 14:28:35 -0500

    importing should not write to disk

--- chipsff/__init__.py
+++ chipsff/__init__.py
@@ -1,2 +1,5 @@
 from .config import CHIPSFFConfig
+# FIXME:  The following triggers download of data from jarvis
+# which is sort of alarming on merely running 'import chipsff'
+# put MaterialsAnalyzer in a separate file, put the data downloads in __main__, or (better) both
 from .run_chipsff import MaterialsAnalyzer
